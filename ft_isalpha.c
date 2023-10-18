5c4524d598fd5e16e8bc7635bcd4c789e08523a3
Ronan Pothier <rpothier@k1r3p7.42mulhouse.fr>
2023-10-18 15:47:12 +0200

    3eme envoi

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,8 +6,25 @@
 /*   By: rpothier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 11:45:11 by rpothier          #+#    #+#             */
-/*   Updated: 2023/10/18 11:48:56 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/10/18 15:43:06 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <ctype.h>
+#include <stdio.h>
 
+int	ft_isalpha(int a)
+{
+	if (a >= 65 && a <= 90 || a >= 97 && a <= 122)
+		return (1);
+	return (0);
+}
 
+/*int	main(void)
+{
+	int	a;
+
+	a = ';';
+	printf("%d\n", ft_isalpha(a));
+	printf("%d\n", isalpha(a));
+	return (0);
+}*/
