b6e39de3fb2a7374cb4d9262e65619d91c4ca188
Ronan Pothier <rpothier@k2r4p5.42mulhouse.fr>
2023-10-19 23:15:16 +0200

    1er strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpothier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 22:06:58 by rpothier          #+#    #+#             */
+/*   Updated: 2023/10/19 23:13:26 by rpothier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	if (c == '\0')
+		return (&s[i]);
+	else
+	{
+		while (s[i] != c && i > 0)
+			i--;
+		if (s[i] == c)
+			return (&s[i]);
+		else
+			return (NULL);
+	}
+}
+
+/*int	main(void)
+{
+	char	*s;
+	int		c;
+
+	s = "Allo oui.";
+	c = 'A';
+	printf("%s\n", ft_strrchr(s, c));
+	printf("%s\n", strrchr(s, c));
+	return (0);
+}*/
