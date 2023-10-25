b3178ddb9a5c893c37592e3059f50c237367e91c
roronan13 <ronan.pothier@gmail.com>
2023-10-25 19:21:23 +0200

    3eme libft.h

--- libft.h
+++ libft.h
@@ -1,10 +1,16 @@
 #ifndef LIBFT_H
 #define LIBHT_H
 
-int ft_isalpha(int);
-int ft_isdigit(int);
-int ft_isalnum(int);
-int ft_isascii(int);
-
+int		ft_isalpha(int);
+int		ft_isdigit(int);
+int		ft_isalnum(int);
+int		ft_isascii(int);
+int		ft_isprint(int);
+size_t	ft_strlen(const char);
+int		ft_toupper(int);
+int		ft_tolower(int);
+char	*ft_strchr(const char *, int);
+char	*ft_strrchr(const char *, int);
+int		ft_strncmp(const char *, const char *, size_t);
 
 #endif
