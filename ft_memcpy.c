06bdd2c42c1317848579d8546a40b301ddc79ed5
roronan13 <ronan.pothier@gmail.com>
2023-10-31 20:19:20 +0100

    oui

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 19:33:55 by rpothier          #+#    #+#             */
-/*   Updated: 2023/10/31 19:44:39 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/10/31 20:17:35 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	char	*ptr;
+	char	*ptr_dest;
+	char	*ptr_src;
 
 	i = 0;
-	ptr = (char *)src;
-	while (i < n && dest[i] && src[i])
+	ptr_dest = (char *)dest;
+	ptr_src = (char *)src;
+	while (i < n && ptr_dest[i] && ptr_src[i])
 	{
-		dest[i] = ptr[i];
+		ptr_dest[i] = ptr_src[i];
 		i++;
 	}
-	return (dest);
+	return (ptr_dest);
 }
