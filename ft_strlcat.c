c92d190df070c3c3bd9f76696cd577b95c311ae6
roronan13 <ronan.pothier@gmail.com>
2023-10-31 15:25:35 +0100

    oui

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 16:25:00 by rpothier          #+#    #+#             */
-/*   Updated: 2023/10/31 14:57:07 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/10/31 15:15:00 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	{
 		while (dst[i])
 			i++;
-		while (src[j] && j < size - 1 && i < size - 1)
+		while (src[j] && i < size - 1)
 		{
 			dst[i] = src[j];
 			i++;
