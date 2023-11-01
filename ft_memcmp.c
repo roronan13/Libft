480e9fb5191690c12950d9599a01525f1413bdcd
roronan13 <ronan.pothier@gmail.com>
2023-11-01 23:23:46 +0100

    oui

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:03:50 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/01 23:05:10 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/01 23:23:42 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	
+	size_t			i;
+	unsigned char	*ptr1;
+	unsigned char	*ptr2;
+
+	i = 0;
+	ptr1 = (unsigned char *)s1;
+	ptr2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (ptr1[i] && ptr2[i] && i < n && ptr1[i] == ptr2[i])
+		i++;
+	return (*ptr1[i] - *ptr2[i]);
 }
