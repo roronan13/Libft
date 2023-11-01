0b2f327cfd7b7fbecb709850cb10c90cf90818ad
roronan13 <ronan.pothier@gmail.com>
2023-11-01 19:56:23 +0100

    oui

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 16:58:33 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/01 18:43:06 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/01 19:56:00 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,26 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	size_t	i;
 	char	*ptr_dest;
 	char	*ptr_src;
+	size_t	j;
 
 	i = 0;
+	j = 0;
 	ptr_dest = (char *)dest;
 	ptr_src = (char *)src;
 	if (!dest && !src)
 		return (NULL);
+	if (ft_strlen(ptr_src) > ft_strlen(ptr_dest))
+	{
+		i = ft_strlen(src) - 1;
+		j = ft_strlen(dest) - 1;
+		while (i - n >= 0)
+		{
+			ptr_dest[j] = ptr_src[i];
+			i--;
+			j--;
+		}
+		return (ptr_dest);
+	}
 	while (i < n)
 	{
 		ptr_dest[i] = ptr_src[i];
