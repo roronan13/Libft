462ab8f4060fa73b328e19a677d1a66216b12c71
roronan13 <ronan.pothier@gmail.com>
2023-11-01 17:00:47 +0100

    oui

--- Makefile
+++ Makefile
@@ -9,6 +9,7 @@ SRCS	=	ft_isalpha.c\
 			ft_memset.c\
 			ft_bzero.c\
 			ft_memcpy.c\
+			ft_memmove.c\
 			ft_strlcpy.c\
 			ft_strlcat.c\
 			ft_toupper.c\
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 19:26:47 by rpothier          #+#    #+#             */
-/*   Updated: 2023/10/31 19:36:10 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/01 17:00:14 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t a);
 size_t	ft_strlcat(char *dst, const char *src, size_t a);
 int		ft_toupper(int a);
