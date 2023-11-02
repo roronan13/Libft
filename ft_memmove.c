be3ac5a7a5605baed4ff426186678e3eaf93f3b7
roronan13 <ronan.pothier@gmail.com>
2023-11-02 14:16:05 +0100

    oui

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 16:58:33 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/02 12:34:25 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/02 14:15:33 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,24 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-	unsigned char	*ptr_dest;
+	size_t				i;
+	unsigned char		*ptr_dest;
 	unsigned const char	*ptr_src;
-	size_t	j;
-	size_t	k;
+	size_t				j;
 
-	i = 0;
-	j = 0;
-	k = 0;
+	i = n - 1;
+	j = n - 1;
 	ptr_dest = (unsigned char *)dest;
 	ptr_src = (unsigned const char *)src;
-	if (!dest && !src)
-		return (NULL);
-	//if (ft_strlen(ptr_src) > ft_strlen(ptr_dest))
 	if (ptr_src < ptr_dest)
-	//if (&ptr_src < &ptr_dest)
 	{
-		i = n - 1;
-		j = n - 1;
-		while (k < n)
+		while (n)
 		{
-			ptr_dest[j] = ptr_src[i];
-			i--;
-			j--;
-			k++;
+			ptr_dest[j--] = ptr_src[i--];
+			n--;
 		}
 		return (ptr_dest);
 	}
-	else
-	{
-		while (i < n)
-		{
-			ptr_dest[i] = ptr_src[i];
-			i++;
-		}
-		return (ptr_dest);
-	}
+	ptr_dest = ft_memcpy(dest, src, n);
+	return (ptr_dest);
 }
