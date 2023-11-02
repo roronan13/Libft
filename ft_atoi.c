0fc0662f791d84e7fac2d9a3bca2adbbef3459f1
roronan13 <ronan.pothier@gmail.com>
2023-11-02 16:37:43 +0100

    oui

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:04:50 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/02 16:26:39 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/02 16:37:08 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,17 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	resu = 0;
 	a = 1;
-	while (!ft_isdigit(ptr[i]))
+	while (!ft_isdigit(ptr[i] - 48))
 		i++;
-	while (ft_isdigit(ptr[i]))
+	while (ft_isdigit(ptr[i] - 48))
 		i++;
-	while (ft_atoi(ptr[i]))
+	while (ft_isdigit(ptr[i] - 48))
 	{
 		resu += (ptr[i] - 48) * a;
 		a *= 10;
 		i--;
 	}
+	if (ptr[i - 1] == '-')
+		return (-resu);
 	return (resu); 
 }
