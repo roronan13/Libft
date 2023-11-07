55ad261c9e00ad867bcddb0fa9e1e42f0d92e374
roronan13 <ronan.pothier@gmail.com>
2023-11-07 21:26:17 +0100

    oui

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:04:50 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/07 21:08:11 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/07 21:26:02 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	isspace(int a)
+int	ft_isspace(int a)
 {
 	if (a == 32 || (a >= 9 && a <= 13))
 		return (1);
@@ -26,15 +26,16 @@ int	ft_atoi(const char *nptr)
 	int				a;
 	int				resu;
 
-	ptr = (unsigned char*)nptr;
+	ptr = (unsigned char *)nptr;
 	i = 0;
 	resu = 0;
 	a = 1;
-	while (isspace(ptr[i]))
+	while (ft_isspace(ptr[i]))
 		i++;
 	while (ft_isdigit(ptr[i]))
 		i++;
-	while (ft_isdigit(ptr[i]))
+	i -= 1;
+	while (ft_isdigit(ptr[i]) && i >= 0)
 	{
 		resu += (ptr[i] - 48) * a;
 		a *= 10;
@@ -42,5 +43,5 @@ int	ft_atoi(const char *nptr)
 	}
 	if (ptr[i - 1] == '-')
 		return (-resu);
-	return (resu); 
+	return (resu);
 }
