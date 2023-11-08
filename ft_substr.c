527d8832b9a96fc9a253c4286b837c8fc3ab2aca
roronan13 <ronan.pothier@gmail.com>
2023-11-08 17:58:42 +0100

    oui

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:48:39 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/08 16:52:44 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/08 17:52:55 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	char		*ptr;
+	const char	*ptr2;
+
+	if (len > ft_strlen(s) - start)
+		ptr = malloc(sizeof(char) * (ft_strlen(s) - start + 1));
+	else
+		ptr = malloc(sizeof(char) * len + 1);
+	if (!ptr)
+		return (NULL);
+	ft_memcpy(ptr, ptr2, len);
+	return (ptr);
 }
