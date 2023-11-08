ba82abc17927de77b64c5afba82f2369cb91d26f
roronan13 <ronan.pothier@gmail.com>
2023-11-08 19:26:54 +0100

    oui

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:48:39 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/08 17:52:55 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/08 19:19:08 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,29 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char		*ptr;
-	const char	*ptr2;
+	char	*ptr;
+	char	*s_copy;
+	size_t	i;
 
+	i = 0;
+	s_copy = (char *)s;
+	if (start >= ft_strlen(s))
+	{
+		ptr = malloc(sizeof(char) * 1);
+		return (ptr);
+	}
 	if (len > ft_strlen(s) - start)
 		ptr = malloc(sizeof(char) * (ft_strlen(s) - start + 1));
 	else
-		ptr = malloc(sizeof(char) * len + 1);
+		ptr = malloc(sizeof(char) * (len + 1));
 	if (!ptr)
 		return (NULL);
-	ft_memcpy(ptr, ptr2, len);
+	while (s_copy[start] && i < len)
+	{
+		ptr[i] = s_copy[start];
+		i++;
+		start++;
+	}
+	ptr[i] = '\0';
 	return (ptr);
 }
