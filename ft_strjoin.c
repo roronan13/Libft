aa28940b29230a91e9ab43b56d237ef968165116
roronan13 <ronan.pothier@gmail.com>
2023-11-08 20:55:06 +0100

    oui

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 20:16:08 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/08 20:48:08 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/08 20:55:01 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,10 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	//char	*s1_copy;
-	//char	*s2_copy;
 	char	*ptr;
-	size_t	i;
 	size_t	size_s1;
 	size_t	size_s2;
 	
-	i = 0;
 	size_s1 = ft_strlen(s1);
 	size_s2 = ft_strlen(s2);
 	ptr = malloc(sizeof(char) * (size_s1 + size_s2 + 1));
