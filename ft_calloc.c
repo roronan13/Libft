46d3965702858d0a46481c3b8874550757c9dd43
roronan13 <ronan.pothier@gmail.com>
2023-11-08 16:06:00 +0100

    oui

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 14:12:12 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/08 15:19:00 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/08 16:05:00 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 
 	ptr = malloc(size * nmemb);
-	//if (!ptr)
-	//	return (NULL);
-	ft_bzero(ptr, nmemb);
-	//if (nmemb == 0 || size == 0)
-	//	return (NULL);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, nmemb * size);
 	return (ptr);
 }
