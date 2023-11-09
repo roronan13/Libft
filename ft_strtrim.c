734a73b8991c2a21e933c95c20e76a4c85684804
roronan13 <ronan.pothier@gmail.com>
2023-11-09 19:15:44 +0100

    oui

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 15:04:03 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/09 17:59:18 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/09 19:09:49 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	char	*ptr;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = ft_strlen(s1);
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	while (j > i && ft_strchr(set, s1[j - 1]))
+		j--;
+	ptr = malloc(sizeof(char) * (j - i + 1));
+	if (!ptr)
+		return (NULL);
+	ft_memcpy(ptr, &s1[i], j - i + 1);
+	return (ptr);
 }
