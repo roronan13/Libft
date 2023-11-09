b8eae068decb50ae7c787c40cfd1643c3f75c834
roronan13 <ronan.pothier@gmail.com>
2023-11-09 20:42:06 +0100

    oui

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 20:24:34 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/09 20:27:20 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/09 20:41:55 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	size_t	i;
+	char	*ptr;
+	size_t	size;
+
+	i = 0;
+	size = ft_strlen(s) + 1;
+	while (s[i])
+	{
+		f(i, s[i]);
+		i++;
+	}
+	ptr = malloc(sizeof(char) * (size));
+	ptr = ft_strlcpy(ptr, s, size);
+	return (ptr);
 }
