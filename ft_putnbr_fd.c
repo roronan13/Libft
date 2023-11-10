fde140b420c7bf7efa23a1f0785c99671e496d54
roronan13 <ronan.pothier@gmail.com>
2023-11-10 15:44:03 +0100

    allo

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 11:23:55 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/10 15:32:07 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/10 15:43:17 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	else if (n < 0)
 	{
-		n *= - 1;
+		n *= -1;
 		write(fd, "-", 1);
 	}
 	if (n <= 9)
