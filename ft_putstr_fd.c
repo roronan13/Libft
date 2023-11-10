2bd903aa5a45777e240ca46dafad9d05f4971408
roronan13 <ronan.pothier@gmail.com>
2023-11-10 11:07:34 +0100

    oui

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 11:02:06 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/10 11:02:56 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/10 11:07:08 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		write(fd, &s[i++], 1);
 }
