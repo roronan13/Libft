cee806c75383f6cdaf1f15e54c63289619b9f6e4
roronan13 <ronan.pothier@gmail.com>
2023-11-14 16:54:43 +0100

    14 nov

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:50:14 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/14 15:47:23 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/14 16:54:18 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,46 @@ size_t	ft_count(char const *s, char c)
 
 	i = 0;
 	j = 0;
-	if (s[i] == 'c')
+	if (ft_strlen(s) == 0)
+		return (0);
+	if (s[i] == c)
 		i++;
 	while (s[i])
 	{
-		if (s[i] == 'c' && s[i + 1] != 'c' && s[i + 1] != '\0')
+		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
 			j++;
 		i++;
 	}
 	return (j + 2);
 }
 
-char	**ft_split(char const *s, char c)
+/* char	**ft_split(char const *s, char c)
 {
 	char	**ptr;
+	size_t	i;
 
+	i = 0;
 	ptr = (char **)malloc(sizeof(char*) * ft_count(s, c));
+	if (!ptr)
+		return (NULL);
 	
+}
+	
+	if (!ptr[i])
+	{
+		while (i >= 0)
+		{
+			free(ptr[i]);
+			i--;
+		}
+	} */
+
+int main()
+{
+	const char	*s;
+	char		c;
+
+	s = "dfghdf";
+	c = '\0';
+	printf("%ld\n", ft_count(s, c));
 }
