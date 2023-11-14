11609f38ab1371ca34434c4cf17467dc4b4fc84b
roronan13 <ronan.pothier@gmail.com>
2023-11-14 14:24:41 +0100

    14 nov

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,26 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:50:14 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/14 13:51:08 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/14 14:23:49 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	ft_count(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] )
+	}
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+
 }
