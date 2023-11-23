596e641885e0f7713979b37ec260dde22112eb91
roronan13 <ronan.pothier@gmail.com>
2023-11-23 17:30:30 +0100

    23 nov

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 16:20:10 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/23 16:24:48 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/23 17:29:55 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	
+	t_list* new_element = malloc(sizeof(t_list));
+	new_element->content = content;
+	new_element->next = NULL;
+	return new_element;
 }
