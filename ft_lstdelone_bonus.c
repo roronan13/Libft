7e55b2346731a75328e84f2e898931e7480c5cc5
roronan13 <ronan.pothier@gmail.com>
2023-11-30 22:40:21 +0100

    30 nov

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/30 20:02:39 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/30 22:37:20 by rpothier         ###   ########.fr       */
+/*   Created: 2023/11/30 22:37:50 by rpothier          #+#    #+#             */
+/*   Updated: 2023/11/30 22:39:01 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* #include "libft.h"
+#include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	size_t	size;
-	t_list	*new;
-	t_list	*a;
-
-	if (!lst || !f || !del)
-		return (NULL);
-	a = lst;
-	size = ft_lstsize(lst);
-	new = malloc(sizeof(t_list) * (size + 1));
-	if (!new)
-		return (NULL);
-	while (a != NULL)
-	{
-		new->content = f(a->content);
-		new = new->next;
-		a = a->next;
-	}
-	return (new);
-	del(a);
+	
 }
- */
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 19:26:47 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/30 20:06:08 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/30 22:39:26 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
