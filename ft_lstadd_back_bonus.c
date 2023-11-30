cccc7579a13b7f2f2aa521e69ca07f258baf28ba
roronan13 <ronan.pothier@gmail.com>
2023-11-30 18:48:46 +0100

    30 nov

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 17:40:25 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/30 16:46:18 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/30 18:47:40 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*a;
 
-	a = *lst;
-	if (new && lst)
+	if (!lst || !new)
+		return ;
+	if (!*lst)
 	{
-		while (a && a->next != NULL)
-			a = a->next;
-		a->next = new;
-		//new->next = NULL;
-		//*lst = a;
+		*lst = new;
+		return ;
 	}
+	a = ft_lstlast(*lst);
+	a->next = new;
 }
