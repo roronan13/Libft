de9ce35b28da8d40fef3fa07e338e0797a0623f8
roronan13 <ronan.pothier@gmail.com>
2023-11-30 23:23:52 +0100

    30 nov

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 22:37:50 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/30 22:39:01 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/30 23:23:02 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
 }
