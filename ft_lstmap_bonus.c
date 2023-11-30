f62655a377dff18f3b2315149dbc19aee8092d6a
roronan13 <ronan.pothier@gmail.com>
2023-11-30 21:13:21 +0100

    30 nov

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 20:02:39 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/30 20:48:14 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/11/30 21:11:40 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	size_t	size;
 	t_list	*new;
 	t_list	*a;
-	
+
 	if (!lst || !f)
 		return (NULL);
 	a = lst;
