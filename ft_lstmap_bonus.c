5ea1b335dbd196478190a393f95fb97bb5b26bb7
roronan13 <ronan.pothier@gmail.com>
2023-12-01 17:32:35 +0100

    1 dec

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,15 +6,15 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 20:02:39 by rpothier          #+#    #+#             */
-/*   Updated: 2023/11/30 22:37:20 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/12/01 17:23:49 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* #include "libft.h"
+#include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	size_t	size;
+	/* size_t	size;
 	t_list	*new;
 	t_list	*a;
 
@@ -32,6 +32,22 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		a = a->next;
 	}
 	return (new);
-	del(a);
+	del(a); */
+	
+	t_list	primary_node;
+	t_list	second_node;
+	t_list	final_node;
+	t_list	node;
+
+	primary_node = lst;
+	second_node = lst;
+	while (primary_node)
+	{
+		ft_lstiter(primary_node, f);
+		node = ft_lstnew(primary_node->content);
+		if (!node)
+		{
+			
+		}
+	}
 }
- */
