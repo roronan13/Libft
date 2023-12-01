40f2bb4f963430b544d2a5d07ed46d528e923163
roronan13 <ronan.pothier@gmail.com>
2023-12-01 16:02:14 +0100

    1 dec

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 23:25:34 by rpothier          #+#    #+#             */
-/*   Updated: 2023/12/01 15:16:02 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/12/01 16:01:20 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,22 @@
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*node;
-	//t_list	*node_temp;
+	t_list	*node_temp;
 
-	if (!lst)
+	if (!lst || !*lst)
 		return ;
-	if (!*lst || !del)
+	if (!del)
 	{
-		//*lst = NULL;
+		*lst = NULL;
 		return ;
 	}
-	//node = *lst;
-	/* while (node)
-	{
-		node_temp = node;
-		del(node->content);
-		free(node);
-		node = node_temp->next;
-	} */
 	node = *lst;
 	while (node->next)
 	{
 		del(node->content);
-		node = node->next;
+		node_temp = node->next;
+		free(node);
+		node = node_temp;
 	}
 	del(node->content);
 	free(node);
