3d6d248ff8e58455118310e12f6cb3030ad76c66
roronan13 <ronan.pothier@gmail.com>
2023-12-06 21:35:46 +0100

    6 dec

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 20:02:39 by rpothier          #+#    #+#             */
-/*   Updated: 2023/12/06 20:19:49 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/12/06 21:33:29 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,17 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	**node;
-	t_list	*node1;
-	t_list	new;
-	
-	node1 = lst;
-	*node = lst;
-	while (node1)
+	t_list	*node;
+	t_list	*new_node;
+
+	node = NULL;
+	while (lst)
 	{
-		new = ft_lstnew(f(node1->content));
-		if (!new)
-			ft_lstclear(node, del);
-		ft_lstadd_back(node, new);
-		node1 = node1->next;
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
+			ft_lstclear(&node, del);
+		ft_lstadd_back(&node, new_node);
+		lst = lst->next;
 	}
 	return (node);
 }
